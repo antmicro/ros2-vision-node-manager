0958f6c3e7ada8c7787e5bb5eaa866544ae0e594
Illia Vysochyn <ivysochyn@internships.antmicro.com>
2023-05-09 14:06:40 +0200

    [#44523] include: cvnode_manager.hpp: Rework node management to operate on a single service
    Replaces the register and unregister service with a single 'manage' service.
    
    Replaces prepare/process/cleanup services with a single 'communication' service.
    
    Separates the cvnode_manager class into a header and a source `.cpp` file.

--- include/cvnode_manager/cvnode_manager.hpp
+++ include/cvnode_manager/cvnode_manager.hpp
@@ -1,122 +1,47 @@
 #pragma once
 
 #include <rclcpp/rclcpp.hpp>
-#include <std_srvs/srv/trigger.hpp>
 #include <string>
 #include <unordered_map>
 
-#include <cvnode_msgs/srv/register_cv_node.hpp>
-#include <cvnode_msgs/srv/unregister_cv_node.hpp>
+#include <cvnode_msgs/srv/manage_cv_node.hpp>
+#include <cvnode_msgs/srv/runtime_protocol_srv.hpp>
 
 namespace cvnode_manager
 {
 
 /**
- * Structure to hold CVNode's services.
- *
- * @tparam T Type of the service to process an image.
- */
-template <class T> struct CVNodeService
-{
-    /// Client to prepare the Node for segmentation.
-    rclcpp::Client<std_srvs::srv::Trigger>::SharedPtr prepare_service;
-
-    /// Client to process an image.
-    typename rclcpp::Client<T>::SharedPtr process_service;
-
-    /// Client to free Node's resources.
-    rclcpp::Client<std_srvs::srv::Trigger>::SharedPtr cleanup_service;
-};
-
-/**
  * Node to manage testing process of the computer vision system.
- *
- * @tparam T Type of the service to process an image.
  */
-template <class T> class CVNodeManager : public rclcpp::Node
+class CVNodeManager : public rclcpp::Node
 {
 private:
     /**
-     * Callback for the service to register the node.
+     * Callback for the service to manage the BaseCVNode-like nodes.
      *
      * @param request Request of the service for registering the node.
      * @param response Response of the service for registering the node.
      */
-    void register_node_callback(const cvnode_msgs::srv::RegisterCVNode::Request::SharedPtr request,
-                                cvnode_msgs::srv::RegisterCVNode::Response::SharedPtr response)
-    {
-        std::string node_name = request->node_name;
-
-        RCLCPP_INFO(this->get_logger(), "Registering the node '%s'", node_name.c_str());
-
-        // Check if the node is already registered
-        if (cv_nodes.find(node_name) != cv_nodes.end())
-        {
-            response->status = false;
-            response->message = "The node is already registered";
-            RCLCPP_ERROR(this->get_logger(), "The node '%s' is already registered", node_name.c_str());
-            return;
-        }
-
-        CVNodeService<T> cv_node_service{};
-
-        response->status = false;
-
-        if (!initialize_service_client<std_srvs::srv::Trigger>(request->prepare_service_name,
-                                                               cv_node_service.prepare_service))
-        {
-            response->message = "Could not initialize the prepare service";
-            return;
-        }
-
-        if (!initialize_service_client<T>(request->process_service_name, cv_node_service.process_service))
-        {
-            response->message = "Could not initialize the process service";
-            return;
-        }
-
-        if (!initialize_service_client<std_srvs::srv::Trigger>(request->cleanup_service_name,
-                                                               cv_node_service.cleanup_service))
-        {
-            response->message = "Could not initialize the close service";
-            return;
-        }
-
-        response->status = true;
-        response->message = "The node is registered";
-
-        cv_nodes[node_name] = cv_node_service;
-
-        RCLCPP_INFO(this->get_logger(), "The node '%s' is registered", node_name.c_str());
-        return;
-    }
+    void manage_node_callback(const cvnode_msgs::srv::ManageCVNode::Request::SharedPtr request,
+                              cvnode_msgs::srv::ManageCVNode::Response::SharedPtr response);
 
     /**
-     * Callback for the service to unregister the node.
+     * Callback for the service to register the BaseCVNode-like nodes.
+     *
+     * @param request Request of the service for registering the node.
+     * @param response Response of the service for registering the node.
+     */
+    void register_node_callback(const cvnode_msgs::srv::ManageCVNode::Request::SharedPtr request,
+                                cvnode_msgs::srv::ManageCVNode::Response::SharedPtr response);
+
+    /**
+     * Callback for the service to unregister the BaseCVNode-like nodes.
      *
      * @param request Request of the service for unregistering the node.
      * @param response Response of the service for unregistering the node.
      */
-    void unregister_node_callback(const cvnode_msgs::srv::UnregisterCVNode::Request::SharedPtr request,
-                                  [[maybe_unused]] cvnode_msgs::srv::UnregisterCVNode::Response::SharedPtr response)
-    {
-
-        std::string node_name = request->node_name;
-
-        RCLCPP_INFO(this->get_logger(), "Unregistering the node '%s'", node_name.c_str());
-
-        // Check if the node is already registered
-        if (cv_nodes.find(node_name) == cv_nodes.end())
-        {
-            RCLCPP_ERROR(this->get_logger(), "The node '%s' is not registered", node_name.c_str());
-            return;
-        }
-
-        cv_nodes.erase(node_name);
-
-        RCLCPP_INFO(this->get_logger(), "The node '%s' is unregistered", node_name.c_str());
-        return;
-    }
+    void unregister_node_callback(const cvnode_msgs::srv::ManageCVNode::Request::SharedPtr request,
+                                  [[maybe_unused]] cvnode_msgs::srv::ManageCVNode::Response::SharedPtr response);
 
     /**
      * Creates a client to a service.
@@ -137,7 +62,7 @@ private:
             return false;
         }
 
-        client = this->create_client<T>(service_name);
+        client = this->create_client<TClient>(service_name);
 
         if (!client->wait_for_service(std::chrono::seconds(1)))
         {
@@ -148,34 +73,21 @@ private:
     }
 
     /// Service to register the CVNode.
-    rclcpp::Service<cvnode_msgs::srv::RegisterCVNode>::SharedPtr register_service;
+    rclcpp::Service<cvnode_msgs::srv::ManageCVNode>::SharedPtr manage_service;
 
-    /// Service to unregister the CVNode.
-    rclcpp::Service<cvnode_msgs::srv::UnregisterCVNode>::SharedPtr unregister_service;
-
-    std::unordered_map<std::string, CVNodeService<T>> cv_nodes; ///< Map of registered nodes.
+    /// Map of registered nodes.
+    std::unordered_map<std::string, rclcpp::Client<cvnode_msgs::srv::RuntimeProtocolSrv>::SharedPtr> cv_nodes;
 
 public:
     /**
      * Constructor.
      *
      * @param node_name Name of the node.
-     * @param register_service_name Name of the service to register nodes.
-     * @param unregister_service_name Name of the service to unregister nodes.
+     * @param manage_service_name Name of the service to manage BaseCVNode-like nodes.
      * @param options Node options.
      */
-    CVNodeManager(const std::string node_name, const std::string &register_service_name,
-                  const std::string &unregister_service_name, const rclcpp::NodeOptions &options)
-        : Node(node_name, options)
-    {
-        register_service = this->create_service<cvnode_msgs::srv::RegisterCVNode>(
-            register_service_name,
-            std::bind(&CVNodeManager::register_node_callback, this, std::placeholders::_1, std::placeholders::_2));
-
-        unregister_service = this->create_service<cvnode_msgs::srv::UnregisterCVNode>(
-            unregister_service_name,
-            std::bind(&CVNodeManager::unregister_node_callback, this, std::placeholders::_1, std::placeholders::_2));
-    }
+    CVNodeManager(const std::string node_name, const std::string &manage_service_name,
+                  const rclcpp::NodeOptions &options);
 };
 
 } // namespace cvnode_manager
---
+++ src/cvnode_manager.cpp
@@ -0,0 +1,88 @@
+#include <cvnode_manager/cvnode_manager.hpp>
+
+namespace cvnode_manager
+{
+
+CVNodeManager::CVNodeManager(const std::string node_name, const std::string &manage_service_name,
+                             const rclcpp::NodeOptions &options)
+    : Node(node_name, options)
+{
+    manage_service = create_service<cvnode_msgs::srv::ManageCVNode>(
+        manage_service_name,
+        std::bind(&CVNodeManager::manage_node_callback, this, std::placeholders::_1, std::placeholders::_2));
+}
+
+void CVNodeManager::manage_node_callback(const cvnode_msgs::srv::ManageCVNode::Request::SharedPtr request,
+                                         cvnode_msgs::srv::ManageCVNode::Response::SharedPtr response)
+{
+    if (request->type == request->REGISTER)
+    {
+        register_node_callback(request, response);
+    }
+    else if (request->type == request->UNREGISTER)
+    {
+        unregister_node_callback(request, response);
+    }
+    else
+    {
+        RCLCPP_ERROR(get_logger(), "Unknown type of the service");
+        response->status = false;
+        response->message = "Unknown type of the service";
+    }
+    return;
+}
+
+void CVNodeManager::register_node_callback(const cvnode_msgs::srv::ManageCVNode::Request::SharedPtr request,
+                                           cvnode_msgs::srv::ManageCVNode::Response::SharedPtr response)
+{
+    std::string node_name = request->node_name;
+    RCLCPP_INFO(get_logger(), "Registering the node '%s'", node_name.c_str());
+
+    response->status = false;
+
+    // Check if the node is already registered
+    if (cv_nodes.find(node_name) != cv_nodes.end())
+    {
+        response->message = "The node is already registered";
+        RCLCPP_ERROR(get_logger(), "The node '%s' is already registered", node_name.c_str());
+        return;
+    }
+
+    // Create a client to communicate with the node
+    rclcpp::Client<cvnode_msgs::srv::RuntimeProtocolSrv>::SharedPtr cv_node_service;
+    if (!initialize_service_client<cvnode_msgs::srv::RuntimeProtocolSrv>(request->srv_name, cv_node_service))
+    {
+        response->message = "Could not initialize the communication service client";
+        RCLCPP_ERROR(get_logger(), "Could not initialize the communication service client");
+        return;
+    }
+
+    response->status = true;
+    response->message = "The node is registered";
+    cv_nodes[node_name] = cv_node_service;
+
+    RCLCPP_INFO(get_logger(), "The node '%s' is registered", node_name.c_str());
+}
+
+void CVNodeManager::unregister_node_callback(
+    const cvnode_msgs::srv::ManageCVNode::Request::SharedPtr request,
+    [[maybe_unused]] cvnode_msgs::srv::ManageCVNode::Response::SharedPtr response)
+{
+    std::string node_name = request->node_name;
+
+    RCLCPP_INFO(get_logger(), "Unregistering the node '%s'", node_name.c_str());
+
+    // Check if the node is already registered
+    if (cv_nodes.find(node_name) == cv_nodes.end())
+    {
+        RCLCPP_ERROR(get_logger(), "The node '%s' is not registered", node_name.c_str());
+        return;
+    }
+
+    cv_nodes.erase(node_name);
+
+    RCLCPP_INFO(get_logger(), "The node '%s' is unregistered", node_name.c_str());
+    return;
+}
+
+} // namespace cvnode_manager
